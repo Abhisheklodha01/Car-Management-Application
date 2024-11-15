58b119a9ae0007037269dc97f3f21a73ea542367
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-15 16:43:48 +0530

    created footer page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-52'
-    >Footer</div>
+    <div className='h-44'
+    >
+        <div className='text-gray-700'>
+          <p className='text-center text-xl pt-10'>Contect Us</p>
+          <ul className='flex justify-center items-center flex-col md:flex-row mt-5 gap-4 md:gap-10'>
+            <li>Email: support@carstore.com</li>
+            <li>LinkedIn</li>
+            <li>Github</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            </ul> 
+        </div>
+    </div>
   )
 }
 
