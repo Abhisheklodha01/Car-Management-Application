2abe1665ef755f787efa9cf590227e8635859e82
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-15 13:51:35 +0530

    established connection with databse

---
+++ server/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import {config} from 'dotenv'
+
+config()
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
+        console.log("database connected");
+        console.log(connectionInstance.connection.host);
+
+    } catch (error) {
+        console.log("Database connection failed", error);
+        process.exit(1)
+
+    }
+
+}
+
+export default connectDB
