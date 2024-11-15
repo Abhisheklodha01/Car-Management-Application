d0341c84f0924e0a7cf65f05479a74f06f3dc2d0
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-15 14:02:00 +0530

    setup routing

---
+++ server/routes/user.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+    ForgotPasswordController,
+    GetUserProfileController,
+    Logincontroller,
+    Registercontroller,
+} from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
+import { searchJob } from "../controllers/jobs.controller.js";
+
+const router = Router();
+
+router.post("/signup", Registercontroller);
+router.post("/login", Logincontroller);
+router.put("/forgot-password", ForgotPasswordController);
+router.get("/user-profile", isAuthenticated, GetUserProfileController);
+router.get("/jobs", searchJob)
+
+
+export default router
