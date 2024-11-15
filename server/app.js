0a67640dfb06c061e1da334df3fae34354a62f2d
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-15 14:04:05 +0530

    added cors policy and manged routes

--- server/app.js
+++ server/app.js
@@ -8,16 +8,22 @@ config()
 
 
 const app = express()
-// app.use(cors)
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 app.use(express.json())
 app.use(cookieParser())
 
+connectDB()
 
 app.get("/", (req, res) => {
     res.send("Server is working fine")
 })
 
-connectDB()
+app.use("/api/users", userRouter);
 
 
 export default app
