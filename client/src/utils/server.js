c77650601dabd02aa03ec36d06a9bf58d7ee0903
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-16 22:31:11 +0530

    added deployed server url

--- client/src/utils/server.js
+++ client/src/utils/server.js
@@ -1,1 +1,2 @@
-export const backendUrl = "http://localhost:5000/api"
+// export const backendUrl = "http://localhost:5000/api"
+export const backendUrl = "https://car-management-application-s2dr.onrender.com/api"
