dd2a80b9ead56447bdf20cc00ce845b9739d2e04
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-11-16 15:44:51 +0530

    adding contex authentication and logout fucntionality

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import { userContex } from "../contex/UserContex.js";
+import { TbUserEdit } from "react-icons/tb";
+import toast from "react-hot-toast";
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+  const { isAthenticated } = useContext(userContex);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    toast.success("Logout Successfully", {
+      position: "top-center",
+    });
+    navigate("/login");
+  };
+
   return (
     <div className="h-16 flex flex-row justify-between items-center">
       <div className="">
@@ -20,20 +33,37 @@ const NavBar = () => {
           Upload Cars
         </Link>
       </div>
-      <div>
-        <Link to={"/login"} className="mr-5">
-          <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
-            <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
-            <span className="relative z-20">Login</span>
-          </button>
-        </Link>
-        <Link to={"/signup"} className="md:mr-5">
-          <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
-            <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
-            <span className="relative z-20">Signup</span>
-          </button>
-        </Link>
-      </div>
+      {isAthenticated ? (
+        <div>
+          <Link to={"/login"} className="mr-5">
+            <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+              <span className="relative z-20">Login</span>
+            </button>
+          </Link>
+          <Link to={"/signup"} className="md:mr-5">
+            <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+              <span className="relative z-20">Signup</span>
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to={"/upload-cars"} className="mr-3">
+            <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+              <span className="relative z-20">Upload</span>
+            </button>
+          </Link>
+          <Link to={"/view-cars"} className="md:mr-5">
+            <button className="px-6 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+              <span className="relative z-20">View Cars</span>
+            </button>
+          </Link>
+        </div>
+      )}
       <div
         onClick={() => setNav(!nav)}
         className="md:hidden cursor-pointer pr-4 z-30 text-gray-700"
@@ -50,13 +80,30 @@ const NavBar = () => {
           className="flex flex-col z-20 justify-center items-center absolute top-0 left-0
          w-full h-screen bg-slate-800 text-gray-200"
         >
-          <Link to={"/"} className="mb-10">
-            Home
-          </Link>
-          <Link to={"/view-cars"} className="mb-10">
-            View Cars
-          </Link>
-          <Link to={"/upload-cars"}>Upload Cars</Link>
+          {isAthenticated === true ? (
+            <div className=" flex flex-col justify-center items-center">
+              <Link to={"/"} className="mb-10">
+                Home
+              </Link>
+              <Link to={"/view-cars"} className="mb-10">
+                View Cars
+              </Link>
+              <Link className="mb-10" to={"/upload-cars"}>
+                Upload Cars
+              </Link>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <div className=" flex flex-col justify-center items-center">
+              <Link to={"/"} className="mb-10">
+                Home
+              </Link>
+              <Link to={"/view-cars"} className="mb-10">
+                Login
+              </Link>
+              <Link to={"/upload-cars"}>Signup</Link>
+            </div>
+          )}
         </div>
       )}
     </div>
