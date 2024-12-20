fd2b63ccc28726568d83564a3640d592adefa487
Abhisheklodha01 <abhisheklodha512@gmail.com>
2024-12-20 23:36:52 +0530

    added cluster

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 import { config } from 'dotenv'
 import app from './app.js'
+import os from 'os'
+import cluster from 'cluster'
 
 config()
 
 const port = process.env.PORT || 5000
 
+const CPU = os.cpus().length
+if (cluster.isPrimary) {
+  for (let i = 0; i < CPU; i++) {
+    cluster.fork()
+  }
+}
+else {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    
 })
+}
